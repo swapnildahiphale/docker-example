46294a34cc787dda208682735c8953374c965f3b
Mukta Aphale <MAphale@whitehedge.com>
2016-04-18 19:13:54 +0530

    Fixed bugs

--- node/index.js
+++ node/index.js
@@ -4,18 +4,15 @@ var express = require('express'),
 
 var app = express();
 
-console.log(process.env.REDIS_PORT + ':' + process.env.REDIS_ADDRESS);
+console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 
-var client = redis.createClient(
-	process.env.REDIS_PORT || 6379,
-  	process.env.REDIS_ADDRESS || redis
-);
-
+var client = redis.createClient('6379', 'redis');
 
 app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
     if(err) return next(err);
-    res.send('This page has been viewed ' + counter + ' times!');
+    var ip = process.env.MY_NAME;
+    res.send('This page has been viewed ' + counter + ' times!<br><br>Request processed by ' + ip + '.');
   });
 });
 
