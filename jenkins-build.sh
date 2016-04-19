e9a1b1f48368ab1118faaeb72a9d10407b2de375
Mukta Aphale <MAphale@whitehedge.com>
2016-04-19 14:14:12 +0530

    Added jenkins build script

---
+++ jenkins-build.sh
@@ -0,0 +1,12 @@
+#/bin/bash +x
+
+docker-compose --project-name=${JOB_NAME} stop &> /dev/null || true &> /dev/null
+docker-compose --project-name=${JOB_NAME} rm --force &> /dev/null || true &> /dev/null
+docker stop `docker ps -a -q -f status=exited` &> /dev/null || true &> /dev/null
+docker rm -v `docker ps -a -q -f status=exited` &> /dev/null || true &> /dev/null
+docker rmi `docker images --filter 'dangling=true' -q --no-trunc` &> /dev/null || true &> /dev/null
+
+
+docker-compose build
+
+docker-compose --project-name=${JOB_NAME} up &
